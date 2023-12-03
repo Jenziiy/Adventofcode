660e4f1a3f73ec9cea3c6be49145e2808994a52c
user.email <developerjenzii@gmail.com>
2023-12-03 21:23:00 +0100

    Day 2 finish game 2

--- src/main/kotlin/com/example/adventofcode/cubeGameDayTwo/CubeGame.kt
+++ src/main/kotlin/com/example/adventofcode/cubeGameDayTwo/CubeGame.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 class CubeGame {
 
-    fun getFileContents(): MutableMap<String, String> {
+    private fun getFileContents(): MutableMap<String, String> {
         val gameMap = mutableMapOf<String, String>()
         val myFile = File("cubedata.txt")
         val bufferedReader = myFile.bufferedReader()
@@ -21,24 +21,37 @@ class CubeGame {
         var sumIDs = 0
         val gameMap = getFileContents()
         var sumEligibleIDs = 0
-        gameMap.forEach{ it ->
+        var sumCubes = 0
+        gameMap.forEach{
             val gameID = it.key
             var eligibleGame = true
             val game = it.value.split(";").toTypedArray()
+            var red = 0
+            var green = 0
+            var blue = 0
             game.forEach{ it2 ->
                 val bag = it2.split(",").toTypedArray()
                 bag.forEach {it3 ->
                     val cube = it3.split(" ")
                     if (cube[2] == "red"){
+                        if(red < cube[1].toInt()){
+                            red = cube[1].toInt()
+                        }
 //                     println("${cube[2]} ${cube[1]}")
                         if(cube[1].toInt() > 12 ){
                          eligibleGame = false
                         }
                     } else if(cube[2] == "green"){
+                        if(green < cube[1].toInt()){
+                            green = cube[1].toInt()
+                        }
                      if(cube[1].toInt() > 13 ){
                          eligibleGame = false
                      }
                     } else if(cube[2] == "blue"){
+                        if(blue < cube[1].toInt()){
+                            blue = cube[1].toInt()
+                        }
                      if(cube[1].toInt() > 14 ){
                          eligibleGame = false
                      }
@@ -48,6 +61,7 @@ class CubeGame {
                     }
                 }
             }
+            sumCubes += (red * green * blue)
             if (eligibleGame) {
                 sumEligibleIDs += gameID.split(" ")[1].toInt()
             }
@@ -55,5 +69,6 @@ class CubeGame {
         }
         println("This  is the sum of all VALID IDs $sumEligibleIDs")
         println("This is the sum of ALL IDs: $sumIDs")
+        println("This is the sum of ALL Powers: $sumCubes")
     }
 }
